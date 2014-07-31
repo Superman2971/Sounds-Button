9e441516f781c445a201bd39b306dbd1001d16f6
Superman2971 <goldfarb.ian@gmail.com>
2014-07-31 16:07:55 -0700

    Had to fix the Finding Nemo soundtrack name

--- button.js
+++ button.js
@@ -6,7 +6,7 @@ soundApp.controller("soundController", function($scope){
   {name:"TD4W", location:"audio/td4w.mp3"},
   {name:"300", location:"audio/300.mp3"},
   {name:"Battlestar Galactic", location:"audio/bsg.mp3"},
-  {name:"Finding Nemo", location:"audio/emo.mp3"},
+  {name:"Finding Nemo", location:"audio/nemo.mp3"},
   {name:"Portal 2", location:"audio/portal2.mp3"},
   {name:"Hitchikers Guide to the Galaxy", location:"audio/galaxy.mp3"}
   ];
