2eeb86aae9b5ebbad08f9b5d695464e6c745391d
Superman2971 <goldfarb.ian@gmail.com>
2014-07-31 15:02:07 -0700

    Using ngOptions and a select tag to decide on the sound clip, easy now to add new files and options

--- button.js
+++ button.js
@@ -2,29 +2,19 @@ var soundApp = angular.module("soundApp", []);
 
 soundApp.controller("soundController", function($scope){
 
-  var snd = new Audio("td4w.mp3");
+  $scope.audio = [
+  {name:"TD4W", location:"audio/td4w.mp3"},
+  {name:"300", location:"audio/300.mp3"},
+  {name:"Battlestar Galactic", location:"audio/bsg.mp3"},
+  {name:"Finding Nemo", location:"audio/emo.mp3"},
+  {name:"Portal 2", location:"audio/portal2.mp3"},
+  {name:"Hitchikers Guide to the Galaxy", location:"audio/galaxy.mp3"}
+  ];
 
-  $scope.td4w = function(){
-    snd = new Audio("td4w.mp3");
-  }
+  $scope.myAudio = $scope.audio[0];
 
-  $scope.the300 = function(){
-    snd = new Audio("300.mp3");
-  }
-
-  $scope.bsg = function(){
-    snd = new Audio("bsg.mp3");
-  }
-
-  $scope.nemo = function(){
-    snd = new Audio("nemo.mp3");
-  }
-
-  $scope.portal2 = function(){
-    snd = new Audio("portal2.mp3");
-  }
-
-  $scope.play = function(){
+  $scope.play = function(myAudio){
+    var snd = new Audio(myAudio.location)
     snd.play();
   }
 });
